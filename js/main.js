d5e46a4d4fa7df52c4c1e62e04fad69fcd2ad7cd
Thomas Klemz <tjklemz@gmail.com>
2022-12-28 11:26:06 -0800

    updated colors, simplified code

--- js/main.js
+++ js/main.js
@@ -5,10 +5,10 @@ const draw = SVG().addTo('#music')
   .size('100%', '100%')
   .viewbox(`-${radius} -${radius} ${diameter} ${diameter}`)
 
-const outerThickness = 17
+const outerThickness = 20
 const outerStrokeMiddle = radius - outerThickness / 2
 
-const musicCircle = draw.circle(diameter - outerThickness).attr({
+draw.circle(diameter - outerThickness).attr({
   cx: 0,
   cy: 0,
   fill: 'none',
@@ -20,28 +20,24 @@ const notes = ['F', 'C', 'G', 'D', 'A', 'E', 'B', 'F♯', 'D♭', 'A♭', 'E♭'
 const major6thScale = notes.map((_, i) => i < 7 || i === 9)
 
 function createCoords(radius, i) {
-  var inc = 2*Math.PI / 12;
-  var radians = -Math.PI + inc*i;
+  const inc = 2*Math.PI / 12;
+  const radians = -Math.PI + inc*i;
 
-  return {
-    x: radius*Math.cos(radians),
-    y: radius*Math.sin(radians),
-    angle: radians*180/Math.PI,
-    radians,
-  }
+  return [
+    radius*Math.cos(radians),
+    radius*Math.sin(radians),
+  ]
 }
 
 const notchesGroup = draw.group().attr({
   stroke: 'var(--background-color, black)',
+  'stroke-width': 0.66,
 })
 
-const major6thScaleGroup = draw.group().attr({
-  stroke: 'var(--notes-color, white)',
-})
+const major6thScaleGroup = draw.group()
 
 const notesGroup = draw.group().attr({
-  'font-size': '40%',
-  'font-family': '"Courier New", "Noto Music", sans-serif',
+  'font-size': `${2*outerThickness}%`,
   'text-anchor': 'middle',
   'fill': 'var(--notes-color, white)',
   'dominant-baseline': 'middle',
@@ -49,37 +45,27 @@ const notesGroup = draw.group().attr({
 })
 
 for (const [i, name] of notes.entries()) {
-  const {x, y} = createCoords(outerStrokeMiddle, i)
+  const [x, y] = createCoords(outerStrokeMiddle, i)
   
-  notesGroup.text(name).attr({
-    x,
-    y,
-  })//.click(() => rotateScale(i))
+  notesGroup.text(name).attr({x, y})
 
-  notchesGroup.line(x, y, 0.75*x, 0.75*y)
+  const circleRatio = outerThickness / diameter
+  const extension = 1 - circleRatio - .08
+  notchesGroup.line(x, y, extension*x, extension*y)
 
   if (major6thScale[i]) {
     major6thScaleGroup.circle(0.75*outerThickness).attr({
       cx: x,
       cy: y,
       fill: i % 3 === 0 ? 'var(--dissonance-color, #aaa)' : 'var(--consonance-color, #555)',
-      'stroke-width': 0.2,
+      stroke: i == 1 ? 'var(--notes-color, white)' : 'none',
+      'stroke-width': i == 1 ? 0.9*circleRatio : 0,
+      'stroke-dasharray': i == 1 ? 5.9*circleRatio : 'none',
     })
   }
 }
 
-function rotateScale(i) {
-  major6thScaleGroup.animate().transform({
-    rotate: createCoords(outerStrokeMiddle, i - 7).angle,
-    origin: [0, 0],
-  })
-}
-
-const dragState = {
-  x: 0,
-  y: 0,
-  valid: false,
-}
+const dragState = {}
 
 function calcCoords (e) {
   const rect = e.currentTarget.getBoundingClientRect();
@@ -106,28 +92,18 @@ draw.on('mousemove', function (e) {
 
   const [x, y] = calcCoords(e)
 
-  // const radians = Math.acos(
-  //   (dragState.x * x + dragState.y * y) /
-  //   (Math.sqrt(dragState.x*dragState.x + dragState.y*dragState.y) * Math.sqrt(x*x + y*y))
-  // )
   const radians = Math.atan2(y, x) - Math.atan2(dragState.y, dragState.x)
   const angle = Number.isNaN(radians) ? 0 : radians * 180 / Math.PI
-  //console.log('angle', angle)
 
-  major6thScaleGroup.transform({
-    rotate: angle,
-    origin: [0, 0],
-  }, true)
+  major6thScaleGroup.transform({rotate: angle}, true)
 
   dragState.x = x
   dragState.y = y
 })
 
 draw.on('mouseup', function (e) {
-  const i = Math.round(12 * major6thScaleGroup.transform('rotate') / 360)
-  console.log('i', i)
-  major6thScaleGroup.animate().transform({
-    rotate: i*360/12
-  })
+  const angle = major6thScaleGroup.transform('rotate')
+  const i = Math.round(12 * angle / 360)
+  major6thScaleGroup.animate().transform({rotate: i*360/12})
   dragState.valid = false
 })
